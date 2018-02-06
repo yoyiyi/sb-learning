94eb2a9e261c3629245895068ff7155043117c15
zzq <2455676683@qq.com>
2018-02-06 17:07:42 +0800

    yoyiyi

--- ch01/src/main/java/com/example/helloworld/HelloWorldController.java
+++ ch01/src/main/java/com/example/helloworld/HelloWorldController.java
@@ -14,6 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/")
     public String sayHello() {
-        return "sss";
+        return "Hello World";
     }
 }
