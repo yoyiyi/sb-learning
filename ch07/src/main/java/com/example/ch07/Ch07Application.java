78d641ec77e53c4776468b8afbaaf6ffd57b0210
zzq <2455676683@qq.com>
2018-02-07 16:04:46 +0800

    yoyiyi

---
+++ ch07/src/main/java/com/example/ch07/Ch07Application.java
@@ -0,0 +1,12 @@
+package com.example.ch07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch07Application.class, args);
+	}
+}
