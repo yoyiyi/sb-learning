664c78066e08e004fbfcec15a3330e917db0e548
zzq <90755>
2018-02-09 23:08:52 +0800

    yoyiyi

---
+++ ch10/src/main/java/com/example/ch10/config/WebSocketConfig.java
@@ -0,0 +1,14 @@
+package com.example.ch10.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 类名：WebSocketConfig
+ * 描述：TODO
+ * 时间：2018/2/9 22:31
+ *
+ * @author <a href="2455676683@qq.com">zzq</a>
+ */
+@Configuration
+public class WebSocketConfig  {
+}
